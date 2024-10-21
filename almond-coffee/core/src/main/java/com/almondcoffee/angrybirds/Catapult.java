75f97c1df021e048f5f26b615cdbc3af900dd94d
aakanksha-27 <163016647+aakanksha-27@users.noreply.github.com>
2024-10-22 01:49:58 +0530

    Add files via upload

---
+++ almond-coffee/core/src/main/java/com/almondcoffee/angrybirds/Catapult.java
@@ -0,0 +1,12 @@
+package com.almondcoffee.angrybirds;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Catapult {
+    Sprite catapult;
+
+    public Catapult(){
+        catapult = new Sprite(new Texture("catapultstretched.png"));
+    }
+}
